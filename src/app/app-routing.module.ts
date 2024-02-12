b6037855a6f4778dbdc845206518b2ba80887ebd
amr <amr.casper.98@gmail.com>
2024-02-12 22:39:38 +0200

    .htaccess file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,7 +33,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top', useHash: true })],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top', useHash: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
